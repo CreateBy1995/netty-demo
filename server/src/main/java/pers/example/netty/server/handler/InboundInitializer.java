addc4edc5afd4b1898fda807462d51661b157183
CreateBy1995 <592329774@qq.com>
2024-07-10 22:23:08 +0800

    pipeline测试

--- server/src/main/java/pers/example/netty/server/handler/InboundInitializer.java
+++ server/src/main/java/pers/example/netty/server/handler/InboundInitializer.java
@@ -10,7 +10,10 @@ import io.netty.channel.embedded.EmbeddedChannel;
  */
 public class InboundInitializer extends ChannelInitializer<EmbeddedChannel> {
     @Override
-    protected void initChannel(EmbeddedChannel ch) throws Exception {
+    protected void initChannel(EmbeddedChannel ch){
+        // ChannelPipeline中介绍了处理器的执行顺序
+        // 如果是入站的请求的话是从头结点开始执行的，也就是A -> B
+        // 如果是出站的请求是从尾节点开始执行的 也就是 B -> A
         ch.pipeline().addLast(new InboundHandlerA());
         ch.pipeline().addLast(new InboundHandlerB());
         ch.pipeline().addLast(new OutboundHandlerA());
